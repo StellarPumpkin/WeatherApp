cd84ac6689d5bf1a8c2341c0cb199439b7b0d29a
Lena Ermolaeva <natemikele@gmail.com>
2019-05-26 12:18:06 +0200

    Create weather app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-class App extends Component {
+const Apikey = "43a152006868d5e42d45038e38fca475";
+
+class App extends React.Component {
+  state = {
+
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    
+    const city = e.target.elements.city.value;
+    e.preventDefault();
+    const apicall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}`);
+    const response =await apicall.json();
+    this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+  error: ""
+})
+    
+    console.log(response);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <div><Titles /></div>
+        <div><Form loadWeather={this.getWeather} /></div>
+        <div><Weather 
+        temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}/></div>
+        
       </div>
-    );
+    )
   }
 }
-
 export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class Titles extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>So you want to know the weather?</h1>
+            <p>Type a name of the city then!</p>
+        </div>
+    )
+}
+        
+   
+}
+export default Titles;
